b1050a1ccf989a45edef16172c39518c6ea0d3bf
zaynkorai <zaynulabdin313@gmail.com>
2025-07-08 15:44:02 +0500

    Add dashboard template

---
+++ web/src/layouts/nav-config-workspace.tsx
@@ -0,0 +1,24 @@
+import type { WorkspacesPopoverProps } from './components/workspaces-popover';
+
+// ----------------------------------------------------------------------
+
+export const _workspaces: WorkspacesPopoverProps['data'] = [
+  {
+    id: 'account-1',
+    name: 'Account 1',
+    plan: 'Free',
+    logo: '/assets/icons/workspaces/logo-1.webp',
+  },
+  {
+    id: 'account-2',
+    name: 'Account 2',
+    plan: 'Pro',
+    logo: '/assets/icons/workspaces/logo-2.webp',
+  },
+  {
+    id: 'account-3',
+    name: 'Account 3',
+    plan: 'Pro',
+    logo: '/assets/icons/workspaces/logo-3.webp',
+  },
+];
